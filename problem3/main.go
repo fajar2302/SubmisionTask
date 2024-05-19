9ecef5e242b3eba58ffe93c6d64f85f885c3b3b2
Fajar <fyasa1658@gmail.com>
2024-05-19 12:42:14 +0800

    problem3 solved

---
+++ problem3/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var angka int
+	var isPrime bool = true
+
+	fmt.Println("Masukkan angka yang ingin dicek: ")
+	fmt.Scan(&angka)
+	if angka <= 1 {
+		isPrime = false
+	} else {
+		for i := 2; i*i <= angka; i++ {
+			if angka%i == 0 {
+				isPrime = false
+				break
+			}
+		}
+	}
+
+	fmt.Println(isPrime)
+}
