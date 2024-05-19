e554f6dc2814f9eb3082c43485858ad1ea26ea3a
Fajar <fyasa1658@gmail.com>
2024-05-19 11:19:13 +0800

    problem1 solved

--- problem1/main.go
+++ problem1/main.go
@@ -1,9 +1,25 @@
 package main
 
 import "fmt"
-func main() {
-	var nama string
-	fmt.Scanln(&nama)
 
-	fmt.Println("Halo "+ nama +"!saya Golang bahasa yang sangat menyenangkan")
+func main() {
+	var nilai float32
+	var predikat string
+	fmt.Println("Masukkan Nilai: ")
+	fmt.Scanln(&nilai)
+
+	if (nilai >= 80 && nilai <= 100){
+		predikat = "A"
+	} else if (nilai >= 65 && nilai < 80 ) {
+		predikat = "B+"	
+	} else if (nilai >= 50 && nilai < 65) {
+		predikat = "B"
+	} else if (nilai >= 35 && nilai < 50) {
+		predikat = "C"
+	} else if (nilai >= 0 && nilai < 35) {
+		predikat = "D"
+	}
+
+	fmt.Println("Predikatnnya: ", predikat)
+
 }
