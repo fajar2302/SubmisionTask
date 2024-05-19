2cc052be65577e0bbb5165311752d0f0536fe417
Fajar <fyasa1658@gmail.com>
2024-05-19 15:07:51 +0800

    problem6 solved

---
+++ problem6/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Fungsi untuk mengecek apakah sebuah bilangan adalah bilangan prima
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// Fungsi untuk mengecek apakah digit dalam sebuah bilangan adalah bilangan prima
+func isFullPrime(n int) bool {
+	for n > 0 {
+		digit := n % 10
+		if !isPrime(digit) {
+			return false
+		}
+		n /= 10
+	}
+	return true
+}
+
+func main() {
+	var number int
+	fmt.Print("Masukkan bilangan: ")
+	fmt.Scan(&number)
+
+
+	fmt.Println(isFullPrime(number))
+}
