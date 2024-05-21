65aa4ed8c74c0c41ada027cf6d7838bfd72aa9ab
Fajar <fyasa1658@gmail.com>
2024-05-21 21:31:43 +0800

    perbaikan problem6

--- problem6/main.go
+++ problem6/main.go
@@ -9,7 +9,7 @@ func isPrime(n int) bool {
 	if n <= 1 {
 		return false
 	}
-	for i := 2; i*i <= n; i++ {
+	for i := 2; i < n; i++ {
 		if n%i == 0 {
 			return false
 		}
@@ -17,8 +17,12 @@ func isPrime(n int) bool {
 	return true
 }
 
-// Fungsi untuk mengecek apakah digit dalam sebuah bilangan adalah bilangan prima
+// // Fungsi untuk mengecek apakah digit dalam sebuah bilangan adalah bilangan prima
 func isFullPrime(n int) bool {
+	if !isPrime(n) {
+		return false
+	}
+
 	for n > 0 {
 		digit := n % 10
 		if !isPrime(digit) {
@@ -28,7 +32,6 @@ func isFullPrime(n int) bool {
 	}
 	return true
 }
-
 func main() {
 	var number int
 	fmt.Print("Masukkan bilangan: ")
